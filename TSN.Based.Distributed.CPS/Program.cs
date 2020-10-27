9cb6ee0d455071b5002fe15b610256d445f31077
Jonathan Friis <Jonathan.Friis@outlook.dk>
2020-10-27 11:21:32 +0100

    Fixed functionality for the XML reader.

--- TSN.Based.Distributed.CPS/Program.cs
+++ TSN.Based.Distributed.CPS/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using TSN.Based.Distributed.CPS.Models;
+
 
 namespace TSN.Based.Distributed.CPS
 {
@@ -6,7 +9,14 @@ namespace TSN.Based.Distributed.CPS
     {
         static void Main(string[] args)
         {
-            new Project2();
+            List<Link> links = new List<Link>();
+            List<Device> devices = new List<Device>();
+            List<Stream> streams = new List<Stream>();
+
+            (devices, links, streams) = XmlReader.LoadXml();
+
+
+            Console.Read();
         }
     }
 }
