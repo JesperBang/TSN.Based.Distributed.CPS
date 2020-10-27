67873392ba22f849a001874a682602a608818cd8
Duran Köse <s147153@student.dtu.dk>
2020-10-27 11:17:16 +0100

    Udkast til randomstate

---
+++ TSN.Based.Distributed.CPS/RandomState.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TSN.Based.Distributed.CPS
+{
+    class RandomState
+    {
+        /*
+        public List<Stream> generateState(List<Stream> stream, List<Link> links, List<Device> devices)
+        {
+            int num_stream = stream.Count;
+            int num_links = links.Count;
+            int num_devices = devices.Count;
+
+            create a List<Stream> random_stream
+            of the same size as the incoming stream
+                
+            
+
+            
+            for (i to num_stream) {
+                //add route to stream, a route includes links
+                random_stream[i] = findPath(stream[i].getSrc(), stream[i].getDst(), links, devices);
+                
+                
+            }
+            
+            
+            return random_stream;
+
+        */
+
+
+    }
+
+
+}
+}
