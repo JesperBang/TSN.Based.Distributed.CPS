b57db783c2bbf57245844c6aca008ee2834e6c51
Duran Köse <s147153@student.dtu.dk>
2020-11-01 16:16:18 +0100

    cleaned up

--- UnitTestTSN/BandwidthTest.cs
+++ UnitTestTSN/BandwidthTest.cs
@@ -17,15 +17,6 @@ namespace UnitTestTSN
         [ClassInitialize]
         public static void BeforeClass(TestContext tc)
         {
-            link1 = new Link();
-            link2 = new Link();
-            link3 = new Link();
-            link4 = new Link();
-            link5 = new Link();
-            link6 = new Link();
-            link7 = new Link();
-            link8 = new Link();
-
             stream0 = new Stream();
             stream0.streamId = "Stream0";
             stream0.source = "ES1";
@@ -44,42 +35,45 @@ namespace UnitTestTSN
             stream1.deadline = 10000;
             stream1.rl = 2;
 
-
-
-
-
+            link1 = new Link();
             link1.source = "ES1";
             link1.destination = "SW0";
             link1.speed = 0.25;
 
+            link2 = new Link();
             link2.source = "ES2";
             link2.destination = "SW0";
             link2.speed = 1.25;
 
+            link3 = new Link();
             link3.source = "ES1";
             link3.destination = "SW1";
             link3.speed = 1.25;
 
+            link4 = new Link();
             link4.source = "ES2";
             link4.destination = "SW1";
             link4.speed = 1.25;
 
+            link5 = new Link();
             link5.source = "SW0";
             link5.destination = "ES3";
             link5.speed = 1.25;
 
+            link6 = new Link();
             link6.source = "SW0";
             link6.destination = "ES4";
             link6.speed = 1.25;
 
+            link7 = new Link();
             link7.source = "SW1";
             link7.destination = "ES3";
             link7.speed = 1.25;
 
+            link8 = new Link();
             link8.source = "SW1";
             link8.destination = "ES4";
             link8.speed = 1.25;
-
 
             route1 = new Route();
             route1.links = new List<Link>();
@@ -105,11 +99,7 @@ namespace UnitTestTSN
             routes = new List<Route>();
             routes.Add(route1);
             routes.Add(route2);
-            routes.Add(route3);
-
-
-
-            
+            routes.Add(route3);        
         }
 
         [TestInitialize]
