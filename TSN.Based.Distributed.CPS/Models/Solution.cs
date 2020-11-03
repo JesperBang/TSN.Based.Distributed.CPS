bcc96748521bb1bfe1057b6c2f10b55cc9454c33
Jesper Bang <jesper.xenia@gmail.com>
2020-11-03 10:02:25 +0100

    Created cost function

---
+++ TSN.Based.Distributed.CPS/Models/Solution.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TSN.Based.Distributed.CPS.Models
+{
+    public class Solution
+    {
+        public string StreamId { get; set; }
+        public List<Route> Route { get; set; }
+        public double Cost { get; set; }
+    }
+}
