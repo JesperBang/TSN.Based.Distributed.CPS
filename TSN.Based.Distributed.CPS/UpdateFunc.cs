66f31e0fa74399ef8eb6989678c21cdfc88973a1
Jonathan Friis <Jonathan.Friis@outlook.dk>
2020-11-07 08:59:25 +0100

    UpdatedFunc now has comments

--- TSN.Based.Distributed.CPS/UpdateFunc.cs
+++ TSN.Based.Distributed.CPS/UpdateFunc.cs
@@ -12,27 +12,32 @@ namespace TSN.Based.Distributed.CPS
         {
             PathFinder pf = new PathFinder();
             Random rnd = new Random();
+            // find random stream
             int streamRandom = rnd.Next(0, solutions.Count);
             Solution currStream = solutions[streamRandom];
+
+            // find random route for the specific stream
             int routeCount = currStream.Route.Count;
             int routeRandom = rnd.Next(0, routeCount);
             Route currRoutes = currStream.Route[routeRandom];
 
+            // find random link for the specific route
             int linkToReplaceFrom = rnd.Next(0, currRoutes.links.Count);
+
+            // make new route with the remnants from the old route that should still be used
             Route newRoute = new Route();
             newRoute.links = currRoutes.links.GetRange(0, currRoutes.links.Count - (currRoutes.links.Count - linkToReplaceFrom));
-            //currRoutes.links.RemoveRange(linkToReplaceFrom, currRoutes.links.Count - linkToReplaceFrom);
-           
+            
+            // call path method with the source of the random link and the destination of the stream
             string src = currRoutes.links[linkToReplaceFrom].source;
             string dest = solutions[streamRandom].Route[routeRandom].dest;
-            
             Route path = pf.FindPath(src, dest, links, devices, new List<string>(), new Route());
 
-
+            // add the newly found path to the route
             newRoute.links.AddRange(path.links);
 
-            solutions[streamRandom].Route.RemoveAt(routeRandom);
-            solutions[streamRandom].Route.Add(newRoute);
+            // remove old route and add new to solution
+            solutions[streamRandom].Route[routeRandom] = newRoute;
 
             return solutions;
         }
