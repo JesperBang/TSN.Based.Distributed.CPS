fc513c1a020469e4eaab3de174a95a15baf6b99a
Jesper Bang <jesper.xenia@gmail.com>
2020-11-10 09:28:56 +0100

    Doing SA

--- TSN.Based.Distributed.CPS/CostFunction.cs
+++ TSN.Based.Distributed.CPS/CostFunction.cs
@@ -1,48 +1,64 @@
 ﻿using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using TSN.Based.Distributed.CPS.Models;
 
 namespace TSN.Based.Distributed.CPS
 {
     public class CostFunction
     {
-        public double CalcCostFunction(Solution input) 
+        /// <summary>
+        /// Calculates cost of List<Solution>
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns>Cost as a double</returns>
+        public double CalcCostFunction(List<Solution> input) 
         {
             LinkUtil lu = new LinkUtil();
             double totalCost = 0;
             int BandTerm;
             int OverlapTerm;
-            double LenTerm = 0;
-            
 
-            Dictionary<string, int> linkmap = new Dictionary<string, int>();
-
-            // bandwidth ok?
-            BandTerm = lu.IsBandwidthExceeded(input, input.Route) ? 1 : 0;
-
-            // Overlapping links
-            foreach (Route route in input.Route)
+            foreach (Solution sol in input)
             {
-                foreach (Link link in route.links)
+                double LenTerm = 0;
+                Dictionary<string, int> linkmap = new Dictionary<string, int>();
+
+                // Use LinkUtil to test bandwidth
+                BandTerm = lu.IsBandwidthExceeded(sol, sol.Route) ? 1 : 0;
+
+                // Overlapping links
+                foreach (Route route in sol.Route)
                 {
-                    if (linkmap.ContainsKey(link.source + link.destination)) linkmap[link.source + link.destination] += 1;
-                    if (!linkmap.ContainsKey(link.source + link.destination)) linkmap.Add(link.source + link.destination, 1);    
+                    foreach (Link link in route.links)
+                    {
+                        if (linkmap.ContainsKey(link.source + link.destination)) linkmap[link.source + link.destination] += 1;
+                        if (!linkmap.ContainsKey(link.source + link.destination)) linkmap.Add(link.source + link.destination, 1);
+                    }
+                    LenTerm += route.links.Count;
                 }
-                LenTerm += route.links.Count;
+
+                OverlapTerm = linkmap.Where(links => links.Value > 1).Count();
+
+                // Average length of routes
+                LenTerm = LenTerm / sol.Route.Count();
+
+                // Total cost calc
+                totalCost += CostCalc(BandTerm, OverlapTerm, LenTerm);
+                sol.Cost = CostCalc(BandTerm, OverlapTerm, LenTerm);
             }
-
-            OverlapTerm = linkmap.Where(links => links.Value > 1).Count();
-
-            // Average length of routes
-            LenTerm = LenTerm / input.Route.Count();
-
-            totalCost = CostCalc(BandTerm, OverlapTerm, LenTerm);
-            input.Cost = totalCost;
 
             return totalCost;
         }
 
+        /// <summary>
+        /// Takes term inputs and return cost based on variables in App.config
+        /// </summary>
+        /// <param name="BandTerm"></param>
+        /// <param name="OverlapTerm"></param>
+        /// <param name="LenTerm"></param>
+        /// <returns>Returns cost for item as double</returns>
         public double CostCalc(double BandTerm, int OverlapTerm, double LenTerm)
         {
             return double.Parse(ConfigurationManager.AppSettings.Get("w1")) * BandTerm + 
---
+++ TSN.Based.Distributed.CPS/SimulatedAnnealing.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TSN.Based.Distributed.CPS.Models;
+
+namespace TSN.Based.Distributed.CPS
+{
+    public class SimulatedAnnealing
+    {
+        public SimulatedAnnealing(int xml)
+        {
+            RandomState random = new RandomState();
+            PathFinder path = new PathFinder();
+            List<Link> links = new List<Link>();
+            List<Device> devices = new List<Device>();
+            List<Stream> streams = new List<Stream>();
+            (devices, links, streams) = XmlReader.LoadXml();
+
+            List<Solution> s_best = random.generateState(streams, links, devices);
+
+            double temp = 5000000;
+            double r = 0.003;
+            int nonew = 0;
+
+            CostFunction cf = new CostFunction();
+            UpdateFunc uf = new UpdateFunc();
+            double c_best = cf.CalcCostFunction(s_best);
+
+            while (temp > 1)
+            {
+                List<Solution> s_new = uf.updateSolution(s_best, links, devices);
+
+
+
+            }
+
+
+
+        }
+    }
+}
