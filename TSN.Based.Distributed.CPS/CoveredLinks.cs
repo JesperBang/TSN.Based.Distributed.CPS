09ea946c67548aff425bef014fdb31b6835e8021
Jesper Bang <jesper.xenia@gmail.com>
2020-12-06 13:55:42 +0100

    fix temp

--- TSN.Based.Distributed.CPS/CoveredLinks.cs
+++ TSN.Based.Distributed.CPS/CoveredLinks.cs
@@ -12,15 +12,15 @@ namespace TSN.Based.Distributed.CPS
             PathFinder path = new PathFinder();
             foreach (Solution sol in input)
             {
-                    List<Link> allLinks = links.FindAll(l => sol.Route.Exists(r => r.links.Contains(l)));
-                    foreach (Link l in allLinks.ToList())
-                    {
-                        allLinks.Remove(l);
-                        List<Route> allRoutes = path.FindAllPaths(sol.source, sol.destination, allLinks, devices);
-                        if (allRoutes.Count < 1) count++;
+                List<Link> allLinks = links.FindAll(l => sol.Route.Exists(r => r.links.Contains(l)));
+                foreach (Link l in allLinks.ToList())
+                {
+                    allLinks.Remove(l);
+                    List<Route> allRoutes = path.FindAllPaths(sol.source, sol.destination, allLinks, devices);
+                    if (allRoutes.Count < 1) count++;
 
-                        allLinks.Add(l);
-                    }
+                    allLinks.Add(l);
+                }
             }
             return count;
         }
@@ -31,16 +31,28 @@ namespace TSN.Based.Distributed.CPS
             PathFinder path = new PathFinder();
             foreach (Solution sol in input)
             {
-                    List<Link> allLinks = links.FindAll(l => sol.Route.Exists(r => r.links.Contains(l)));
+                List<Link> allLinks = links.FindAll(l => sol.Route.Exists(r => r.links.Contains(l)));
 
-                    for (int i = 0; i < allLinks.Count - 1; i++)
+                for (int i = 0; i < allLinks.Count - 1; i++)
+                {
+                    List<Link> temp = allLinks.FindAll(l => (allLinks[i].source != l.source && allLinks[i].destination != l.destination) ||
+                                     (allLinks[i + 1].source != l.source && allLinks[i + 1].destination != l.destination));
+
+                    // dunno
+                    for (int counter = 0; counter < temp.Count(); counter++)
                     {
-                        List<Link> temp = allLinks.FindAll(l => (allLinks[i].source != l.source && allLinks[i].destination != l.destination) ||
-                                         (allLinks[i + 1].source != l.source && allLinks[i + 1].destination != l.destination));
-                        List<Route> allRoutes = path.FindAllPaths(sol.source, sol.destination, temp, devices);
-
-                        if (allRoutes.Count < 1) count++;
+                        allLinks.Remove(temp[counter]);
                     }
+
+                    List<Route> allRoutes = path.FindAllPaths(sol.source, sol.destination, temp, devices);
+
+                    foreach (Link link in temp)
+                    {
+                        allLinks.Add(link);
+                    }
+
+                    if (allRoutes.Count < 1) count++;
+                }
             }
             return count;
         }
